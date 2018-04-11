79834f8fca6c3f93b8566df108f128ae0189a2c8
Eugene ETK <etk@sitecore.net>
2018-04-11 16:53:20 +0800

    created replacement for webedit:preview command; replace the SelectModeRequest hook for the same webedit:preview

---
+++ src/Sitecore.Support.215864/Preview.cs
@@ -0,0 +1,60 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Publishing;
+using Sitecore.Shell.Applications.WebEdit.Commands;
+using Sitecore.Shell.DeviceSimulation;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Text;
+using Sitecore.Web;
+using Sitecore.Web.UI.Sheer;
+using System.Linq;
+
+namespace Sitecore.Support.Shell.Applications.WebEdit.Commands
+{
+  public class Preview : Sitecore.Shell.Applications.WebEdit.Commands.Preview
+  {
+    public override void Execute(CommandContext context)
+    {
+      Assert.ArgumentNotNull(context, "context");
+      if (context.Items.Length >= 1)
+      {
+        Context.ClientPage.Start(this, "Run");
+      }
+      //base.Execute(context);
+    }
+    protected override void Run(ClientPipelineArgs args)
+    {
+      Assert.ArgumentNotNull(args, "args");
+      if (WebUtil.GetQueryString("mode") == "edit")
+      {
+        CheckModifiedParameters parameters = new CheckModifiedParameters
+        {
+          DisableNotifications = true
+        };
+        if (!SheerResponse.CheckModified(parameters))
+        {
+          return;
+        }
+      }
+      WebUtil.SetCookieValue("sc_last_page_mode_command", base.Name);
+
+      UrlString url = WebEditCommand.GetUrl();
+
+      Assert.ArgumentNotNull(url, "url");
+      PreviewManager.RestoreUser();
+      DeviceSimulationUtil.DeactivateSimulators();
+      url["sc_mode"] = "preview";
+      url["sc_debug"] = "0";
+      url["sc_trace"] = "0";
+      url["sc_prof"] = "0";
+      url["sc_ri"] = "0";
+      url["sc_rb"] = "0";
+      if (url.Parameters.AllKeys.Contains<string>("sc_version"))
+      {
+        url.Remove("sc_version");
+      }
+      WebEditCommand.Reload(url);
+
+      base.Run(args);
+    }
+  }
+}
---
+++ src/Sitecore.Support.215864/SelectModeRequest.cs
@@ -0,0 +1,72 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.ExperienceEditor.Utils;
+using Sitecore.Publishing;
+using Sitecore.SecurityModel;
+using Sitecore.Shell.DeviceSimulation;
+using Sitecore.Text;
+using Sitecore.Web;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.SelectMode
+{
+  public class SelectModeRequest : PipelineProcessorRequest<ValueItemContext>
+  {
+    public override PipelineProcessorResponseValue ProcessRequest()
+    {
+      Assert.IsNotNullOrEmpty(base.RequestContext.Value, "Could not get string value for requestArgs:{0}", new object[] { base.Args.Data });
+      string[] strArray = base.RequestContext.Value.Split(new char[] { '|' });
+      if (strArray.Length != 2)
+      {
+        return new PipelineProcessorResponseValue { AbortMessage = "Missing item id or current url" };
+      }
+      string cookieValue = string.Empty;
+      using (new SecurityDisabler())
+      {
+        Item item = Client.CoreDatabase.GetItem(new ID(HttpUtility.UrlDecode(strArray[0])));
+        if (item != null)
+        {
+          cookieValue = item["Message"];
+        }
+      }
+      UrlString url = new UrlString(strArray[1]);
+      switch (cookieValue)
+      {
+        case "webedit:preview":
+          WebUtil.SetCookieValue("sc_last_page_mode_command", cookieValue);
+          ActivatePreview(url);
+          return new PipelineProcessorResponseValue { Value = url.ToString() };
+
+        case "webedit:debug":
+          WebUtil.SetCookieValue("sc_last_page_mode_command", cookieValue);
+          WebEditUtility.ActivateDebug(url);
+          return new PipelineProcessorResponseValue { Value = url.ToString() };
+      }
+      return new PipelineProcessorResponseValue { Value = url.ToString() };
+    }
+    public static void ActivatePreview(UrlString url)
+    {
+      Assert.ArgumentNotNull(url, "url");
+      PreviewManager.RestoreUser();
+      DeviceSimulationUtil.DeactivateSimulators();
+      url["sc_mode"] = "preview";
+      url["sc_debug"] = "0";
+      url["sc_trace"] = "0";
+      url["sc_prof"] = "0";
+      url["sc_ri"] = "0";
+      url["sc_rb"] = "0";
+      if (url.Parameters.AllKeys.Contains<string>("sc_version"))
+      {
+        url.Remove("sc_version");
+      }
+    }
+
+
+
+  }
+}
